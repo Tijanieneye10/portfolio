995704ed67855eae35a7b0b1f3aa446b2d309267
Tijani Usman <brainyworld10@gmail.com>
2021-09-23 23:51:54 +0100

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/tijanieneye10/portfolio'
+      className='link footer__link'
+    >
+      Usman Tijani
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,67 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://tijanieneye10.github.io/portfolio',
+  title: 'UT.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Usman Tijani',
+  role: 'Software Developer',
+  description:
+    'I am a result oriented software developer specialized on Web, Mobile and Desktop. ',
+  resume: '',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/usman-tijani-eneye/',
+    github: 'https://github.com/tijanieneye10',
+  },
+}
+
+const projects = [
+ 
+  {
+    name: 'Kano Economic City',
+    description:
+      'This is the website of the largest market in Western Africa situated in Kano State, Nigeria.',
+    stack: ['WordPress', 'PHP', 'React'],
+    sourceCode: 'https://github.com/tijanieneye10',
+    livePreview: 'https://brainsandhammerskecity.com',
+  },
+  {
+    name: 'Sebede Telecommunications',
+    description:
+      'Utility website and app for sales of Airtime, Data and conversion of airtime to cash.',
+    stack: ['Codeigniter', 'PHP', 'MYSQL'],
+    sourceCode: 'https://github.https://github.com/tijanieneye10',
+    livePreview: 'https://sebededata.com',
+  },
+  {
+    name: 'Dolaway',
+    description:
+      'A cryptocurrency exchange website for buying and selling bitcoin, ethereum and other cryptocurrencies.',
+    stack: ['Laravel', 'Javascript', 'Livewire'],
+    sourceCode: 'https://github.com/tijanieneye10',
+    livePreview: 'https://dolaway.com',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'PHP',
+  'Laravel',
+  'Dart',
+  'Flutter',
+'Vuejs',
+'Codeigniter',
+'PWA',
+'Microsoft Office 365'
+
+]
+
+const contact = {
+  email: 'brainyworld10@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
